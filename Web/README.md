92ce6850d2e2b525bf77353f97bfb11260a1f2bd
Stephen Matis <s.matis@fugro.com>
2021-03-05 16:55:31 -0600

    Protect API and web app with single sign on

---
+++ Web/README.md
@@ -0,0 +1,16 @@
+# AutoTimeLiner Website
+
+Front-end site for accessing the roadmap AWS Lambda. Site is hosted on AWS Amplify. Authentication provided by Auth0.
+
+**Deployment**
+
+Edit `auth_config.json` with the appropriate values.
+
+Zip up the following files & folders Drop the .zip file onto the AWS Amplify console to update the site.
+
+* `css`
+* `js`
+* `node_modules`
+* `auth_config.json`
+* `index.html`
+* `package.json`
---
+++ Web/js/app.js
@@ -0,0 +1,93 @@
+let auth0 = null;
+
+const fetchAuthConfig = () => fetch('/auth_config.json');
+
+const configureClient = async () => {
+    const response = await fetchAuthConfig();
+    const config = await response.json();
+
+    window.apiUrl = config.apiUrl;
+
+    document.getElementById('authenticating').innerHTML = (window.apiUrl.indexOf('<') > -1)
+        ? 'Fill out auth_config.json with the correct values, then refresh.'
+        : 'Verifying authentication for ' + window.apiUrl + '...';
+
+    auth0 = await createAuth0Client({
+        domain: config.domain,
+        client_id: config.clientId,
+        audience: config.audience
+    });
+};
+
+window.onload = async () => {
+    await configureClient();
+
+    updateUI();
+
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        return;
+    }
+
+    // NEW - check for the code and state parameters
+    const query = window.location.search;
+    if (query.includes('code=') && query.includes('state=')) {
+        // Process the login state
+        await auth0.handleRedirectCallback();
+
+        updateUI();
+
+        // Use replaceState to redirect the user away and remove the querystring parameters
+        window.history.replaceState({}, document.title, '/');
+    }
+}
+
+const updateUI = async () => {
+    document.getElementById('authenticating').classList.add('hidden');
+
+    const isAuthenticated = await auth0.isAuthenticated();
+
+    if (isAuthenticated) {
+        document.getElementById('public-content').classList.add('hidden');
+        document.getElementById('instructions').classList.add('hidden');
+        document.getElementById('btn-logout').disabled = false;
+        document.getElementById('btn-login').disabled = true;
+        document.getElementById('btn-logout').classList.remove('hidden');
+        document.getElementById('btn-login').classList.add('hidden');
+
+        document.getElementById('gated-content').classList.remove('hidden');
+
+        const token = await auth0.getTokenSilently();
+        window.token = token;
+    
+        const user = await auth0.getUser();
+
+        const myProfile =
+            '<div style="display: flex; flex-direction: row">' +
+                '<div style="margin-right: 3px"><img src="' + user.picture + '" width="35px" style="border:1px solid black;border-radius:4px" /></div>' +
+                '<div><div>' + user.name + '</div><div>' + user.email + '</div></div>' +
+            '</div>';
+        document.getElementById('my-profile').innerHTML = myProfile;
+      } else {
+        document.getElementById('instructions').classList.remove('hidden');
+        document.getElementById('public-content').classList.remove('hidden');
+        document.getElementById('btn-logout').disabled = true;
+        document.getElementById('btn-login').disabled = false;
+        document.getElementById('btn-logout').classList.add('hidden');
+        document.getElementById('btn-login').classList.remove('hidden');
+        document.getElementById('gated-content').classList.add('hidden');
+      }
+};
+
+const login = async () => {
+    await auth0.loginWithRedirect({
+        redirect_uri: window.location.origin
+    })
+};
+
+const logout = () => {
+    auth0.logout({
+        returnTo: window.location.origin
+    });
+};
